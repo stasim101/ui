967f6c91d29d0b343d5998e76fb8babe192f485e
imran ansari <stasim101@gmail.com>
2021-05-18 20:52:35 +0530

    final commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '../_models/user';
+import { UserService } from '../_services/user.service';
+
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent {
+    loading = false;
+    users: User[];
+
+    constructor(private userService: UserService) { }
+
+    ngOnInit() {
+    }
+}
